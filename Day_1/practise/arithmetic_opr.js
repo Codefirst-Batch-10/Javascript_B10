b5d7a4781b492b6740fd5a5eb022ca251e64a11f
RameshBorate <rameshborate0442@gmail.com>
2025-11-18 15:02:29 +0530

    initial commit

--- Day_1/practise/arithmetic_opr.js
+++ Day_1/practise/arithmetic_opr.js
@@ -1,22 +1,22 @@
 let x=20 , y=10;
 
-// console.log('add: '+ (x+y));
-// console.log('sub: '+ (x-y));
-// console.log('div: '+ (x/y));
-// console.log('mult: '+ (x*y));
-// console.log('mod: '+ (x%y));
-// console.log('inc: '+ (x++));
-// console.log('dec: '+ (y--));
+console.log('add: '+ (x+y)); 
+console.log('sub: '+ (x-y));
+console.log('div: '+ (x/y));
+console.log('mult: '+ (x*y));
+console.log('mod: '+ (x%y));
+console.log('inc: '+ (x++));
+console.log('dec: '+ (y--));
 
-// console.log(true+true); 
-// console.log(true+null);
-// console.log(true+[]);
-// console.log(true*false);
-// console.log(null+null);
-// console.log(null+undefined);
-// console.log(null+NaN);
-// console.log(undefined+undefined);
-// console.log(NaN+NaN);
+console.log(true+true);  // 2
+console.log(true+null); //1
+console.log(true+[]);   //true
+console.log(true*false);    //0
+console.log(null+null);     //0
+console.log(null+undefined);    //nan
+console.log(null+NaN);          //nan
+console.log(undefined+undefined);   //nan
+console.log(NaN+NaN);       //nan
 
 console.log('12+23'+2);      //12+232
 console.log(2-'12');        // -10
