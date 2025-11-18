95f41a13674b4783fcbc9f3ae04eef2b7338433a
shubh <shubham.dhanawade@prosoftapp.com>
2025-11-18 15:50:52 +0530

    Day 1 practice

---
+++ Day_1/Practice/index.js
@@ -0,0 +1,101 @@
+// let num = 10;
+// let newResult = num > 1 ? true : false ;
+// console.log(newResult);
+
+// let x = 10 + 1;
+// console.log(x);
+
+// let text1 = "A";
+// let text2 = "B";
+// let result = text1 < text2 ;
+// console.log(result);
+
+//TERNARY OPERATORS
+
+// let isLoggedin = true;
+// let result = x>=18 ? "Eligible for drive " : "Not eligible for drive";
+// console.log(result);
+
+// if(isLoggedin = false ){
+//     console.log("welcome");
+    
+// }else{
+//     console.log("please login");
+    
+// }
+/* ------------------compairison operator */
+
+// let x1 = 10;
+// let x2 = '10';
+
+// res1 = x1 == x2;
+// res2 = x1 ===x2;
+// console.log(res1);
+// console.log(res2);
+
+
+// let x1 = 5 ;
+// let x2 = 5;
+// res = x1 != x2;
+// console.log(res);
+
+// let x1 = 4;
+// let x2 = 4;
+// res = x1 >= x2; 
+//  console.log(res);
+ 
+
+// let x1 = "apple";
+// let x2 = "banana";
+
+// res = x1 > x2;
+// console.log(res);
+
+
+// let result2 = true == 0;
+// console.log("Is true == 1?", result2);
+
+// let res = false === 0;
+// console.log(res);
+
+// let x1 = 5;
+// let x2 = "10";
+// res = x1 < x2;
+// console.log(res);
+
+// let x1 = "A";
+// let x2 = "a";
+// res = x1 < x2;
+// console.log(res);  //true --> becuse  of ASCII values
+
+// let res = "10" > 2 && 5 < "7";
+// console.log(res);
+
+// let res = null === undefined;
+// console.log(res);
+
+// let res = naN == NaN;
+// console.log(res);
+
+// let res = {} == {};
+// console.log(res);
+
+/***********************************bitwise */
+// var x1 = 2;
+// var x2 = 3;
+
+// console.log(x1 & x2);
+
+
+var test1 = true;
+var test2 = false;
+
+
+console.log(test1 && test2);
+
+console.log(test1 || test2);
+
+console.log(!test2);
+
+
+
