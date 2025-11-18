1eab776144b068e109d3d9293f6aa4eb0a9af5da
Rkulkrni <rahulspk7@gmail.com>
2025-11-18 11:03:43 +0530

    js_basic_18_11_2025

---
+++ Day_1/Study/arithmatic.js
@@ -0,0 +1,34 @@
+var first = 10;
+
+var second = 3;
+
+var add = first + second;
+var sub = first - second;
+var mul = first * second;
+var div = first / second;
+var mod = first % second;
+// console.log(add, sub, mul);
+// console.log(mod); // modulus will always give remainnder
+// pre increment
+/*kjdsbgfdsbf
+sdknfbdsjbfd
+ksdnfdsnf
+kmsdnfdknfkds
+dnfdsknf
+ */
+var inc = ++first;
+var dec = --second;
+//post increment
+var inc1 = first++;
+var dec1 = second--;
+
+console.log(inc1, dec1);
+
+console.log(!true);
+
+const x= "test";
+console.log(typeof(x));
+
+const isData = (3 > 4) ? true : false;
+
+
---
+++ Day_1/Study/syntax.js
@@ -0,0 +1,19 @@
+console.log('Hello this is my first Script from external JS file');
+
+const test = 25;
+console.log(typeof(test));
+
+
+console.log(5 && 5); 
+console.log(5 == '5');
+console.log(5 === '5');
+
+let a = 10;
+if(a > 5) {
+    true;
+} else {
+    false;
+}
+
+// ternery
+let result = a > 5 ? true: false;
