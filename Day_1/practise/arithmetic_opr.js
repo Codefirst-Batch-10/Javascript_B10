a7d1304f3b179e90d9f1654d4b1fc8f470539128
RameshBorate <rameshborate0442@gmail.com>
2025-11-18 14:55:46 +0530

    Arithmatic operator

---
+++ Day_1/practise/arithmetic_opr.js
@@ -0,0 +1,24 @@
+let x=20 , y=10;
+
+// console.log('add: '+ (x+y));
+// console.log('sub: '+ (x-y));
+// console.log('div: '+ (x/y));
+// console.log('mult: '+ (x*y));
+// console.log('mod: '+ (x%y));
+// console.log('inc: '+ (x++));
+// console.log('dec: '+ (y--));
+
+// console.log(true+true); 
+// console.log(true+null);
+// console.log(true+[]);
+// console.log(true*false);
+// console.log(null+null);
+// console.log(null+undefined);
+// console.log(null+NaN);
+// console.log(undefined+undefined);
+// console.log(NaN+NaN);
+
+console.log('12+23'+2);      //12+232
+console.log(2-'12');        // -10
+console.log('22'-12);        // 10
+
