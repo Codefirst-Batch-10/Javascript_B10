8d151181daa62c0be5f19edf48e60e36da9a1f4c
Pradipkakde <kakde.pradip1@gmail.com>
2025-11-19 15:11:43 +0530

    mini calculator project with airthematic operator

---
+++ Day_1/Practice/Calculator.js
@@ -0,0 +1,37 @@
+function calculate(op) {
+      // इनपुट्स वाचणे
+      const a = parseFloat(document.getElementById('num1').value);
+      const b = parseFloat(document.getElementById('num2').value);
+      let res;
+
+      // ऑपरेशन तपासणे
+      switch (op) {
+        case '+':
+          res = a + b;
+          break;
+        case '-':
+          res = a - b;
+          break;
+        case '*':
+          res = a * b;
+          break;
+        case '/':
+          if (b === 0) {
+            res = "भागाकार केले जाऊ शकत नाही (शून्य)";
+          } else {
+            res = a / b;
+          }
+          break;
+        case '%':
+          res = a % b;
+          break;
+        case '**':
+          res = a ** b;
+          break;
+        default:
+          res = "अपरिचित ऑपरेशन";
+      }
+
+      // परिणाम दाखवणे
+      document.getElementById('result').innerText = ` ${a} ${op} ${b} = ${res}`;
+    }
