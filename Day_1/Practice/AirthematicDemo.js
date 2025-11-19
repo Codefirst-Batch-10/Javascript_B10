8020dba3f1e8ed4fd02d7aa1b357697487e36c85
Pradipkakde <kakde.pradip1@gmail.com>
2025-11-19 12:18:16 +0530

    Airthmetic operator practice

--- Day_1/Practice/AirthematicDemo.js
+++ Day_1/Practice/AirthematicDemo.js
@@ -1,121 +1,36 @@
-/**Addition 
-let ab=10;
-let bc=20;
-let sum=ab+bc;
-console.log(sum);
-
-var c=15;
-var c1=15;
-let sum1=c+c1;
-console.log(sum1);
-
-
-//substraction
-let d=22;
-let d1=56;
-let sub=d-d1;
-console.log(sub);
-
-//multiplicaation
- let mult1=156;
- let mult2=120;
- let mult=mult1*mult2;
- console.log(mult);
-
- //division
-
- let x=50;
- let y=30;
- let division=x*y;
- console.log("Division ="+division);
-
- //find reminder
-
- let finder=50;
- let divider=10;
- let reminder=finder/divider;
- console.log("Reminder="+reminder);
-**/
-// if else
- /**let a = 8;
-let b = 2;
-let operator = "/";  // +, -, *, /
-
-if (operator === "+") {
-    console.log(a + b);
-}
-else if (operator === "-") {
-    console.log(a - b);
-} else if (operator === "*") {
-    console.log(a * b);
-} else if (operator === "/") {
-    console.log(a / b);
-} else {
-    console.log("Invalid operator");
+function Addition(){
+    let result=10+5;
+    document.getElementById("output").innerHTML="10+5="+result;
 }
 
-let num=7;
-if(num%2===0)
-{
-console.log("even")
-}else{
-    console.log("odd")
+function Substraction(){
+    let result=10-5;
+    document.getElementById("output").innerHTML="50-2="+result;
+
+}
+function Multiplication(){
+    let result=10*5;
+    document.getElementById("output").innerHTML="10*5="+result;
+
+}
+function Division(){
+    let result=10/5;
+    document.getElementById("output").innerHTML="10/5="+result;
+}
+function Modulus(){
+    let result=10%5;
+    document.getElementById("output").innerHTML="10%5="+result;
 }
 
-
-var first=3;
-var second=10;
-
-var add=first+second;
-var mult=first*second;
-var div=first/second;
-var sub=first-second;
-var mod=first%second;
-
-console.log(add,mult,div,sub,mod);
-
-
-var inc=++first;
-var dec=--second;
-
-let a = 5;
-let b = ++a;
-
-console.log(a); // 6
-console.log(b); // 6
-
-
-///preincrement
-let i=0;
-while(i<5)
-{
-    console.log("value =",++i)
+let incrnumber=1;
+function Increment(){
+    incrnumber++;
+    document.getElementById("output").innerHTML="increment result= "+incrnumber;
 }
 
-//post increment
-let i=0;
-
-while(i>5);
-{
-    console.log("value=",++i)
+let decrement=5;
+function Decrement(){
+    decrement--;
+    document.getElementById("output").innerHTML="Decreent result="+decrement;
 }
-**/
-
-/**function lognumber(){
-    console.log("recived",x)
-}
-let n=10;
-lognumber(++n);**/
-
-function logNumber(x) {
-    console.log("Received:", x);
-}
-
-/**let n = 10;
-logNumber(++n); **/
-
-//const x= "test";
-//console.log(typeof(x));
-
-//const isData = (3 > 4) ? true : false;
 
