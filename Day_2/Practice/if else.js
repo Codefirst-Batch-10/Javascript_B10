0a029cce65ca7b7c939f32ae53bea3f84a88a7fc
Pradipkakde <kakde.pradip1@gmail.com>
2025-11-20 11:50:25 +0530

    Discount programe practice

--- Day_2/Practice/if else.js
+++ Day_2/Practice/if else.js
@@ -27,10 +27,29 @@ else{
     console.log("fail")
 }
 **/
-let age=prompt("enter your age")
+/**let age=prompt("enter your age")
 if(age>18){
     console.log('you are elagible for voting')
 }
 else{
     console.log("you are not elagible for voting")
+}**/
+
+let price=prompt("Enter Price here");
+if(price>1000 && price<2000){
+    console.log("You are elegible for 10% discount")
+}
+else if(price>=2000 && price<4000){
+    console.log("You are elagible for 20% of discount")
+}
+else if(price>=4000 && price<6000)
+{
+    console.log("you are elagible for 30% discont")
+}
+else if(price>=6000 && price<=8000)
+{
+    console.log("you are elagible for 80% Discount")
+}
+else{
+    console.log("You are not elagible for single discount")
 }
