fa8ff0f40300fd96399a03c16063727001e97525
Praddip kakde <kakdepradip21@gmail.com>
2025-12-25 22:57:54 +0530

    using turnery operator

--- Day_1/Practice/Day5/Oddeven.js
+++ Day_1/Practice/Day5/Oddeven.js
@@ -7,3 +7,14 @@ function odd(a){
     }
 }
 console.log(odd(12));
+console.log(odd(13));
+console.log(odd(-25));
+console.log(odd(-24));
+
+function turnary(a){
+    return a%2===0? "evern":"odd"
+
+}
+console.log(turnary(13))
+
+
