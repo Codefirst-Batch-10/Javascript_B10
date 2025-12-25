325d48efc58515f63db571ca6dfc28a8bd823402
Praddip kakde <kakdepradip21@gmail.com>
2025-12-25 22:06:35 +0530

    function calculate the area of rectangle

---
+++ Day_1/Practice/sum.js
@@ -0,0 +1,19 @@
+function Addition(a,b){
+    const sum=a+b;
+    console.log('sumb is',sum)
+    
+}
+Addition(15,12);
+
+function Area(length,width){
+    if(length<=0){
+        throw console.error("Area is negatvie");
+        
+    }
+    else if(width<=0){
+        throw error("Area is negatie")
+    }
+let total=length*width
+console.log(total)
+}
+Area(10,56);
